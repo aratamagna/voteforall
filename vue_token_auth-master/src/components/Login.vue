1a2b8018c368976897a2dc88540bd9e5d4d414e5
cryptointi <cryptointi@gmail.com>
2019-02-15 20:33:29 -0300

    user

--- vue_token_auth-master/src/components/Login.vue
+++ vue_token_auth-master/src/components/Login.vue
@@ -5,7 +5,7 @@
       You need to login first.
     </p>
     <form @submit.prevent="login">
-      <label><input v-model="email" placeholder="email"></label>
+      <label><input v-model="email" placeholder="usuario"></label>
       <label><input v-model="pass" placeholder="password" type="password"></label> <br>
       <button type="submit">login</button>
       <p v-if="error" class="error">Bad login information</p>
