2870979883f30dee4b504a9743d2d13daa0cb3c4
aratamagna <aratamagna@gmail.com>
2019-02-16 01:26:15 -0300

    componente pregunta

--- vue_token_auth-master/src/router/index.js
+++ vue_token_auth-master/src/router/index.js
@@ -6,6 +6,7 @@ Vue.use(VueRouter)
 import auth from './auth'
 import Login from '../components/Login.vue'
 import Dashboard from '../components/Dashboard.vue'
+import Question from '../components/Question.js'
 
 function requireAuth (to, from, next) {
   if (!auth.loggedIn()) {
@@ -21,7 +22,9 @@ function requireAuth (to, from, next) {
 export default new VueRouter({
   mode: 'history',
   routes: [
+    { path: '/', component: Question, beforeEnter: requireAuth },
     { path: '/dashboard', component: Dashboard, beforeEnter: requireAuth },
+    { path: '/question', component: Question, beforeEnter: requireAuth },
     { path: '/login', component: Login },
     { path: '/logout',
       beforeEnter (to, from, next) {
